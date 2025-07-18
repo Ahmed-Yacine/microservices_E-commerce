2711fecc80b7628a10aa24856d3ffd9be017c4cd
Ahmed-Yacine <ay.boutera@esi-sba.dz>
2025-07-18 01:33:40 +0100

    Refactor NATS integration

--- http-api-gateway/src/app.module.ts
+++ http-api-gateway/src/app.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { NatsClientModule } from './nats-client/nats-client.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'MATH_SERVICE',
-        transport: Transport.NATS,
-        options: {
-          servers: ['nats://nats'],
-        },
-      },
-    ]),
-  ],
+  imports: [NatsClientModule],
   controllers: [],
   providers: [],
 })
